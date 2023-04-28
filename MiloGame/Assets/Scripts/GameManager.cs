39fc89e255d9f145c9726c09e298bff9a7c0748d
Noah V <noahvonk28@gmail.com>
2023-04-27 18:59:17 -0600

    Fixed sink issues and added GameManager

--- MiloGame/Assets/Scripts/GameManager.cs
+++ MiloGame/Assets/Scripts/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     // Update is called once per frame
--- MiloGame/Assets/Scripts/Sinking.cs
+++ MiloGame/Assets/Scripts/Sinking.cs
@@ -9,6 +9,11 @@ public class Sinking : MonoBehaviour
     [SerializeField]
     private bool sinking = false;
 
+    [SerializeField]
+    private float speed = 0.02f;
+
+    [SerializeField]
+    private float maxHeight = -2.26154f;
     public bool onGround;
 
     void Start()
@@ -17,22 +22,23 @@ public class Sinking : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (sinking)
         {
             //number too high
-            transform.position += new Vector3 (0.0f, -0.01f, 0.0f);
+            transform.position += new Vector3(0, -speed, 0);
+            GameManager.Instance.Player.transform.position += new Vector3(0, -speed, 0);
         }
-        else if (transform.position.y >= -2.26154)
+        else if (transform.position.y >= maxHeight)
         {
-
+            // stops from going to high
         }
-        else if (sinking == false)
+        else if (!sinking)
         {
             //Problem is when the platform moves, you leave contact, making sinking false
             //Debug.Log("Miracles Happen");
-            //transform.position += new Vector3 (0.0f, 0.01f, 0.0f);
+            transform.position += new Vector3 (0, speed, 0);
         }
     }
 
