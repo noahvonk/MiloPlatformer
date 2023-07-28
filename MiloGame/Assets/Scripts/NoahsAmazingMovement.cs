aaa31b04d5796e92398b118e83b4d7aea975f2b2
MaskedCod <mgsweeney30@gmail.com>
2023-07-27 20:01:59 -0600

    Made climbing, made more Level 3

--- MiloGame/Assets/Scripts/NoahsAmazingMovement.cs
+++ MiloGame/Assets/Scripts/NoahsAmazingMovement.cs
@@ -1,10 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class NoahsAmazingMovement : MonoBehaviour
 {
@@ -15,26 +11,13 @@ public class NoahsAmazingMovement : MonoBehaviour
     /// </summary>
     public void Update()
     {
+
         Move();
 
-        if (dir == direction.RIGHT)
-        {
-            int mask = LayerMask.GetMask("Wall");
-            _hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(1f, 0), .6f, mask);
-            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(.6f, 0, 0));
-        }
-        else
-        {
-            int mask = LayerMask.GetMask("Wall");
-            _hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(-1f, 0), .6f, mask);
-            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(-.6f, 0, 0));
-        }
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
-
     }
 
     /// <summary>
@@ -42,61 +25,106 @@ public class NoahsAmazingMovement : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        Vector2 velo;
+        Vector2 velo = Vector2.zero;
         float move = Input.GetAxis("Horizontal");
-        if (_WallJumping)
+        if (move > 0)
         {
-            if(move > 0)
+            SetDirection(ref dir, direction.RIGHT);
+            transform.localScale = new Vector3(1, 1, 1);
+            int mask = LayerMask.GetMask("Wall");
+            _hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(1f, 0), .6f, mask);
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(.6f, 0, 0));
+            Debug.Log(_rb.velocity + "Test4");
+            if (!_WallJumping)
             {
-                velo = new(Mathf.Max(_rb.velocity.x * (move), _wallJumpStrength), _rb.velocity.y);
+                _canClimb = true;
             }
-            else if(move < 0)
-            {
-                velo = new(Mathf.Min(_rb.velocity.x * (move ), -_wallJumpStrength), _rb.velocity.y);
-            }
-            else
-            {
-                velo = new(Mathf.MoveTowards(_rb.velocity.x, 0, 0.1f), _rb.velocity.y);
-            }
-            
-        }
-        else
-        {
-            if (move > 0)
-            {
-                velo = new(Mathf.Max(_rb.velocity.x * (move), _maxSpeed), _rb.velocity.y);
-            }
-            else if (move < 0)
-            {
-                velo = new(Mathf.Min(_rb.velocity.x * (move), -_maxSpeed), _rb.velocity.y);
-            }
-            else
-            {
-                velo = new(0, _rb.velocity.y);
-            }
-        }
-        
-        if(move > 0)
-        {
-            dir = direction.RIGHT;
         }
         else if (move < 0)
         {
-            dir = direction.LEFT;
+            SetDirection(ref dir, direction.LEFT);
+            transform.localScale = new Vector3(-1, 1, 1);
+            int mask = LayerMask.GetMask("Wall");
+            _hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(-1f, 0), .6f, mask);
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(-.6f, 0, 0));
+            Debug.Log(_rb.velocity + "Test5");
+            if (!_WallJumping)
+            {
+                _canClimb = true;
+            }
         }
+        else
+        {
+            _canClimb = false;
+        }
+        if (_hit.collider != null && _hit.collider.gameObject.CompareTag("Ground"))
+        {
+            _WallJumping = false;
+        }
+
+
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && _canClimb && _hit.collider != null && _hit.collider.gameObject.CompareTag("Ground"))
+        {
+
+            Debug.Log("In Climbing position");
+            velo = Climb();
+            _rb.gravityScale = 0;
+
+        }
+        else
+        {
+            _rb.gravityScale = 1;
+            if (_WallJumping)
+            {
+                if (move > 0)
+                {
+                    velo = new(Mathf.Max(_rb.velocity.x * (move), _maxSpeed), _rb.velocity.y);
+                }
+                else if (move < 0)
+                {
+                    velo = new(Mathf.Min(_rb.velocity.x * (move), -_maxSpeed), _rb.velocity.y);
+                }
+                else
+                {
+                    velo = _rb.velocity;
+                }
+            Debug.Log(_rb.velocity + "Test2");
+            }
+            else
+            {
+                if (move > 0)
+                {
+                    velo = new(Mathf.Max(_rb.velocity.x * (move), _maxSpeed), _rb.velocity.y);
+                }
+                else if (move < 0)
+                {
+                    velo = new(Mathf.Min(_rb.velocity.x * (move), -_maxSpeed), _rb.velocity.y);
+                }
+                else
+                {
+                    velo = new(0, _rb.velocity.y);
+                }
+            Debug.Log(_rb.velocity + "Test3");
+            }
+
+        }
+
 
         if (!_grounded && _inWaterfall && velo.y > _MaxWaterfallDropSpeed)
         {
             Debug.Log("Milo is stinky, but getting clean in the waterfall");
             velo.y -= _WaterfallFallSpeedIncrement;
+            Debug.Log(_rb.velocity + "Test6");
         }
         else if(!_grounded && _inWaterfall)
         {
             velo.y = -_MaxWaterfallDropSpeed;
+            Debug.Log(_rb.velocity + "Test7");
         }
 
-        Debug.Log(velo);
+        //Debug.Log(velo);
         _rb.velocity = velo;
+        Debug.Log(_rb.velocity + "Test8");
     }
     /// <summary>
     /// Checks for jump input and updates the physics system
@@ -113,23 +141,63 @@ public class NoahsAmazingMovement : MonoBehaviour
             Debug.Log(_hit.collider);
             if( !_WallJumping && _hit.collider != null && _hit.collider.gameObject.CompareTag("Ground"))
             {
-                if(dir == direction.LEFT) dir = direction.RIGHT;
-                else if(dir == direction.RIGHT) dir = direction.LEFT;
+                _canClimb = false;
+                ChangeDirection(ref dir);
 
                 if(dir == direction.LEFT)
                 {
-                    _rb.velocity = new Vector2(-_rb.velocity.x, 0);
-                    _rb.AddRelativeForce(new Vector2(-_wallJumpStrength, 8), ForceMode2D.Impulse);
+                    _rb.velocity = new Vector2(-Mathf.Abs(_rb.velocity.x), 0);
+                    _appliedForce = new Vector2(-_wallJumpStrength, 8);
+                    StartCoroutine(WallJumpEnumerator());
                 }
                 else if (dir == direction.RIGHT)
                 {
-                    _rb.velocity = new Vector2(-_rb.velocity.x, 0);
-                    _rb.AddRelativeForce(new Vector2(_wallJumpStrength, 8), ForceMode2D.Impulse);
+                    _rb.velocity = new Vector2(Mathf.Abs(_rb.velocity.x), 0);
+                    _appliedForce = new Vector2(_wallJumpStrength, 8);
+                    StartCoroutine(WallJumpEnumerator());
                 }
                 _WallJumping = true;
                 Debug.Log("wall jumping");
             }
         }
+    }
+
+    private void ChangeDirection(ref direction d)
+    {
+        if(d == direction.LEFT)
+        {
+            d = direction.RIGHT;
+        }
+        else
+        {
+            d = direction.LEFT;
+        }
+        ChangeCharacterLookingDirection();
+    }
+
+    private void SetDirection(ref direction d, direction dir)
+    {
+        d = dir;
+        ChangeCharacterLookingDirection();
+    }
+
+    private void ChangeCharacterLookingDirection()
+    {
+        if(dir == direction.LEFT)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private Vector2 Climb()
+    {
+        Debug.Log("Milo is smell");
+        return new Vector2(_rb.velocity.x, _climbSpeed);
+        Debug.Log(_rb.velocity + "Climb");
     }
 
     // Start is called before the first frame update
@@ -157,6 +225,7 @@ public class NoahsAmazingMovement : MonoBehaviour
         {
             _inWaterfall = true;
         }
+        Debug.Log(_rb.velocity + "TriggerEnter");
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -165,6 +234,7 @@ public class NoahsAmazingMovement : MonoBehaviour
         {
             _inWaterfall = false;
         }
+        Debug.Log(_rb.velocity + "TriggerExit");
     }
 
     private void OnBoxCollidedEntered()
@@ -193,6 +263,16 @@ public class NoahsAmazingMovement : MonoBehaviour
     UnityAction enter;
     UnityAction exit;
 
+    private IEnumerator WallJumpEnumerator()
+    {
+        Vector2.MoveTowards(_appliedForce, new Vector2(0,0), .05f);
+        _rb.AddForce(_appliedForce, ForceMode2D.Impulse);
+        while(_WallJumping && _appliedForce.x != 0 && _appliedForce.y != 0)
+        {
+            yield return null;
+        }
+        
+    }
 
     // serialized fields
 
@@ -212,13 +292,16 @@ public class NoahsAmazingMovement : MonoBehaviour
     private float _wallJumpStrength = 10f;
 
     [SerializeField]
+    private float _climbSpeed = 10f;
+
+    [SerializeField]
     private float _WaterfallFallSpeedIncrement = 2f;
 
     [SerializeField]
     private float _MaxWaterfallDropSpeed = 10f;
     // private fields
 
-    private float _appliedForce = 0.0f;
+    private Vector2 _appliedForce = new Vector2(0.0f, 0.0f);
 
     private Vector3 _ground = Vector3.zero;
 
@@ -228,6 +311,8 @@ public class NoahsAmazingMovement : MonoBehaviour
     private bool _grounded = false;
 
     private Rigidbody2D _rb;
+
+    private bool _canClimb = true;
 
     RaycastHit2D _hit;
 
